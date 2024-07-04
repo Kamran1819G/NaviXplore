970cdb0f6aad5ac2c05ba229c5a5f8a05597ebe4
Kamran Khan <kamrankhan.sde@gmail.com>
2024-07-05 02:18:40 +0530

    Update api_endpoints.dart

--- frontend/app/lib/config/api_endpoints.dart
+++ frontend/app/lib/config/api_endpoints.dart
@@ -12,27 +12,3 @@ class NMMTApiEndpoints{
   static const String GetBusStopsBetweenSoureDestination = "https://nmmtservice.infinium.management/TransistService.asmx/GetStationBetweenSoureDestination";
   static const String GetBusFromSourceToDestination = "https://nmmtservice.infinium.management/TransistService.asmx/GetBusFromSourceToDestination";
 }
-
-class NM_MetroApiEndpoints{
-  static const String GetStations = "https://navixplore.onrender.com/api/nm-metro/stations";
-  static String GetNearestStation (double latitude, double longitude) {
-    return "https://navixplore.onrender.com/api/nm-metro/stations/nearest?latitude=$latitude&longitude=$longitude";
-  }
-  static String SearchStation (String query) {
-    return "https://navixplore.onrender.com/api/nm-metro/stations/search?query=$query";
-  }
-  static String GetUpcomingTrains (String lineID, String direction, String stationID){
-    return "https://navixplore.onrender.com/api/nm-metro/trains/upcoming?lineID=$lineID&direction=$direction&stationID=$stationID";
-  }
-  static String GetMetroSchedule (String lineID, String direction, int trainNo) {
-    return "https://navixplore.onrender.com/api/nm-metro/trains/schedule?lineID=$lineID&direction=$direction&trainNo=$trainNo";
-  }
-  static String GetlineData (String lineID) {
-    return "https://navixplore.onrender.com/api/nm-metro/lines?lineID=$lineID";
-  }
-}
-
-class PlacesApiEndpoints{
-  static const String FamousPlaces = 'https://raw.githubusercontent.com/Kamran1819G/NaviXplore-Website-NextJS/master/src/json/FamousPlaces.json';
-  static const String TouristDestinations = 'https://raw.githubusercontent.com/Kamran1819G/NaviXplore-Website-NextJS/master/src/json/TouristDestinations.json';
-}
